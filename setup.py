2d35c865b9d9a4e15b45b548a15ca68e5ed98c43
Kit Sunde <kitsunde@gmail.com>
2013-07-05 01:23:17 +0800

    We don't depend on django_libs.

--- setup.py
+++ setup.py
@@ -50,8 +50,5 @@ setup(
     url="https://github.com/Celc/jack-bower",
     packages=find_packages(),
     include_package_data=True,
-    install_requires=[
-        'django',
-        'django_libs',
-    ],
+    install_requires=['django'],
 )
