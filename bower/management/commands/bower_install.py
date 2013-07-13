2a9a094ea14a46494d705f6aec6eb8a057aa9e74
Kit Sunde <kitsunde@gmail.com>
2013-07-13 19:23:42 +0800

    Raise an exception when bower can't install.

--- bower/management/commands/bower_install.py
+++ bower/management/commands/bower_install.py
@@ -1,4 +1,5 @@
 import os
+from django.core.management import CommandError
 from bower.management.base import AppDirectoryCommand
 
 
@@ -6,4 +7,6 @@ class Command(AppDirectoryCommand):
     def handle_app(self, app_path):
         if os.path.exists(os.path.join(app_path, 'bower.json')):
             os.chdir(app_path)
-            os.system('bower install')
+            if os.system('bower install') != 0:
+                raise CommandError("Bower encountered an issue while "
+                                   "installing.")
