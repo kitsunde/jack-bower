813e0e1ce3b82c2f41452b4719bdcb05b0512e78
Kit Sunde <kitsunde@gmail.com>
2013-08-25 21:36:26 +0800

    Setup pypi release via make.

--- Makefile
+++ Makefile
@@ -1,3 +1,7 @@
 develop:
 	pip install "file://`pwd`#egg=bower"
 	pip install -e .
+
+
+release:
+	python setup.py sdist upload
--- setup.py
+++ setup.py
@@ -1,31 +1,3 @@
-"""
-Python setup file for the bower app.
-
-In order to register your app at pypi.python.org, create an account at
-pypi.python.org and login, then register your new app like so:
-
-    python setup.py register
-
-If your name is still free, you can now make your first release but first you
-sould check if you are uploading the correct files:
-
-    python setup.py sdist
-
-Inspect the output thoroughly. There shouldn't be any temp files and if your
-app includes staticfiles or templates, make sure that they appear in the list.
-If somethign is wrong, you need to edit MANIFEST.in and run the command again.
-
-If all looks good, you can make your first release:
-
-    python setup.py sdist upload
-
-For new releases, you need to bump the version number in
-bower/__init__.py and re-run the above command.
-
-For more information on creating source distributions, see
-http://docs.python.org/2/distutils/sourcedist.html
-
-"""
 import os
 from setuptools import setup, find_packages
 import bower as app
